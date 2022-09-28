b761c97de1bc8561fa937130b781453ece59487c
kartud <1075238+kartud@users.noreply.github.com>
2022-09-28 14:46:40 -0700

    UI: Send clears text, button disable/re-enable waiting for server response, focus

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,30 @@
 const socket = io();   // Connects to socket.io server
 
+// Elements (DOM element variables prefixed by $ by convention)
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', (message) => {
     console.log(message);
 })
 
-const messageForm = document.querySelector('#message-form');
-const messageInput = document.querySelector('#message-input');
-messageForm.addEventListener('submit', (event) => {
+$messageForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    // Disable form submission while waiting for response
+    $messageFormButton.setAttribute('disabled', 'disabled');
+
     const newText = event.target.elements.message.value.trim();
     if (newText) {
         socket.emit('sendMessage', newText, (error) => {
+            // Got response from server, so re-enable form
+            $messageFormButton.removeAttribute('disabled');
+            // Also, clear sent message text & ensure focus returns to text field.
+            // E.g., in case they clicked the button to send.
+            $messageFormInput.value = '';
+            $messageFormInput.focus();
+
             if (error) {
                 return console.log(error);
             }
@@ -19,16 +33,20 @@ messageForm.addEventListener('submit', (event) => {
     }
 })
 
-const sendLocationButton = document.querySelector('#send-location');
-sendLocationButton.addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.');
     }
+
+    // Disable button until server responds
+    $sendLocationButton.setAttribute('disabled', 'disabled');
+
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude, 
             longitude: position.coords.longitude
         }, () => {
+            $sendLocationButton.removeAttribute('disabled');    // Re-enable button
             console.log('Location shared!');
         });
     })
