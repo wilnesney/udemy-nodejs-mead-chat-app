248ee8f248c30d1939edce754f30fc9e24bcb4bb
kartud <1075238+kartud@users.noreply.github.com>
2022-09-29 11:12:31 -0700

    Fixed a quick bug in getUser()'s return

--- src/utils/users.js
+++ src/utils/users.js
@@ -21,7 +21,7 @@ const addUser = ({ id, username, room }) => {
 
     const user = { id, username, room };
     users.push(user);
-    return user;
+    return { user };
 }
 
 const removeUser = (id) => {
